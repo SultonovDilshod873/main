a2cd18377ec912ac0db3df6f75bfed88f4707661
SultonovDilshod873 <sultonov.d.3639>
2025-04-14 22:43:21 +0500

    2

--- elonlar_doskasi/elonlar/models.py
+++ elonlar_doskasi/elonlar/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+class Elon (models.Model):
+    # E'lon uchun turli maydonlar
+    title= models.CharField(max_length=100) # E'lon nomi 
+    description = models.TextField() # E'lon tavsifi
+    price=models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True) # Narxi
+    created_at=models.DateTimeField(auto_now_add=True) # E'lon yaratish vaqti
+    updated_ad=models.DateTimeField(auto_now=True) # E'lonni oxirgi taxlil qilish vaqti 
+    def __str__(self):
+        return self.title
+    
 
 # Create your models here.
--- elonlar_doskasi/elonlar/views.py
+++ elonlar_doskasi/elonlar/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Elon
+from .forms import ElonForm
 
-# Create your views here.
+def elon_list(request):
+    elonlar = Elon.objects.all()
+    return render(request, 'elonlar/elon_list.html', {'elonlar': elonlar})
+
+def elon_create(request):
+    if request.method == 'POST':
+        form = ElonForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('elon_list')
+    else:
+        form = ElonForm()
+    return render(request, 'elonlar/elon_form.html', {'form': form})
+        
